64f80bec83311721e93b7d5804cc6c89ba1f6dba
TonsV2 <sebastianthegreatful@gmail.com>
2022-07-19 17:13:05 +0700

    refactor: "New*" methods before structs

--- internal/errdef/errdef.go
+++ internal/errdef/errdef.go
@@ -4,34 +4,34 @@ import (
 	"errors"
 )
 
-type duplicated struct{ error }
-
 func NewDuplicated(err error) error {
 	return duplicated{err}
 }
+
+type duplicated struct{ error }
 
 func IsDuplicated(err error) bool {
 	var e duplicated
 	return errors.As(err, &e)
 }
 
-type unauthorized struct{ error }
-
 func NewUnauthorized(err error) error {
 	return unauthorized{err}
 }
+
+type unauthorized struct{ error }
 
 func IsUnauthorized(err error) bool {
 	var e unauthorized
 	return errors.As(err, &e)
 }
 
-type notFound struct{ error }
-
 // NewNotFound creates an error representing a resource that could not be found.
 func NewNotFound(err error) error {
 	return notFound{err}
 }
+
+type notFound struct{ error }
 
 // IsNotFound returns true if err is an error representing a resource that could not be found and false otherwise.
 func IsNotFound(err error) bool {
