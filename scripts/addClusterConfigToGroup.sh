0bf67d6348ce73c9090c72086ed333e94d4de9a5
TonsV2 <sebastianthegreatful@gmail.com>
2022-10-28 13:44:46 +0700

    chore: use mktemp and remove file posting

--- scripts/addClusterConfigToGroup.sh
+++ scripts/addClusterConfigToGroup.sh
@@ -5,8 +5,10 @@ set -euo pipefail
 GROUP=$1
 PLAIN_TEXT_CONFIG_FILE=$2
 
-ENCRYPTED_CONFIG_FILE=/tmp/kubernetes.sops.yaml
+ENCRYPTED_CONFIG_FILE=$(mktemp)
 
 sops -e "$PLAIN_TEXT_CONFIG_FILE" > $ENCRYPTED_CONFIG_FILE
 
 $HTTP --ignore-stdin --form post "$INSTANCE_HOST/groups/$GROUP/cluster-configuration" "kubernetesConfiguration@$ENCRYPTED_CONFIG_FILE" "Authorization: Bearer $ACCESS_TOKEN"
+
+rm $ENCRYPTED_CONFIG_FILE
