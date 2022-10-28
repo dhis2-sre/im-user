fa1710ac43639958e36467b33f32fb5a61d8ae63
TonsV2 <sebastianthegreatful@gmail.com>
2022-10-28 13:51:34 +0700

    chore: simple echo api response rather than formatting as export commands. Not only is this not the response of the scripts, but it also makes them less usable in other scripts

--- scripts/signIn.sh
+++ scripts/signIn.sh
@@ -2,10 +2,4 @@
 
 set -euo pipefail
 
-RESPONSE=$($HTTP --auth "$USER_EMAIL:$PASSWORD" post "$INSTANCE_HOST/tokens")
-
-ACCESS_TOKEN=$(echo "$RESPONSE" | jq -r '.access_token')
-REFRESH_TOKEN=$(echo "$RESPONSE" | jq -r '.refresh_token')
-
-echo "export ACCESS_TOKEN=$ACCESS_TOKEN"
-echo "export REFRESH_TOKEN=$REFRESH_TOKEN"
+$HTTP --auth "$USER_EMAIL:$PASSWORD" post "$INSTANCE_HOST/tokens"
