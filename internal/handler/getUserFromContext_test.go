fb1e5d2aa9974f30273b377eb952e4669084040a
TonsV2 <sebastianthegreatful@gmail.com>
2023-01-06 22:36:23 +0700

    chore: initial tests for GetUserFromContext

---
+++ internal/handler/getUserFromContext_test.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/dhis2-sre/im-user/pkg/model"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+)
+
+func TestGetUserFromContext_Happy(t *testing.T) {
+	id := uint(0)
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Set("user", &model.User{
+		Model: gorm.Model{ID: id},
+	})
+
+	user, err := GetUserFromContext(c)
+
+	assert.NoError(t, err)
+	assert.Equal(t, id, user.ID)
+}
+
+func TestGetUserFromContext_NoUserOnContext(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+
+	_, err := GetUserFromContext(c)
+
+	assert.Error(t, err)
+	assert.True(t, strings.HasPrefix(err.Error(), "unable to extract user from request context for unknown reason"))
+}
+
+func TestGetUserFromContext_InvalidUserOnContext(t *testing.T) {
+	w := httptest.NewRecorder()
+	c, _ := gin.CreateTestContext(w)
+	c.Set("user", "not a user struct")
+
+	_, err := GetUserFromContext(c)
+
+	assert.Error(t, err)
+	assert.True(t, strings.HasPrefix(err.Error(), "unable to cast user for unknown reason"))
+}
